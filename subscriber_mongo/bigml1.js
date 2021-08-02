927a0edb54b2636c1b77b72343b642efee945cfe
or2209 <57071074+or2209@users.noreply.github.com>
2021-08-02 23:01:51 +0300

    fix bigml1

--- subscriber_mongo/bigml1.js
+++ subscriber_mongo/bigml1.js
@@ -27,7 +27,6 @@ source.create('out_file.json', function(error, sourceInfo) {
         var model = new bigml.Model();
          model.create(datasetInfo, function (error, modelInfo) {
           if (!error && modelInfo) {
-            console.log("here we check predic")
 
           var localModel = new bigml.LocalModel(modelInfo.resource);
           localModel.predict({"car_number": event.car_number,
@@ -55,95 +54,3 @@ exports.bigi=bigi
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var bigml = require('bigml');
-// var fs=require('fs')
-// var connection = new bigml.BigML('bigdata9963',
-//                              'f3419d14ef15689226aa92fb8037d30a30c07981',
-                                
-//                              )
-
-//     const FILE_NAME = 'outputmongodb.json';
-
-
-// function getJSONFileData(filename) {
-//   return fs
-//     .readFileSync(filename, 'utf-8')
-//     .split('\n')
-//     .filter(Boolean)
-//     .map(JSON.parse);
-// }
-
-// function mapEvents(events) {
-//   return JSON.stringify(events);
-// }
-// const x = mapEvents(getJSONFileData(FILE_NAME));
-//     const myArray= JSON.parse(x)
-//    // console.log(myArray)
-
-// //    var bigml = require('bigml');
-// //    var localEnsemble = new bigml.LocalEnsemble(['model/558c231a9dbfae1ae9c62ab081a083edb857d563']);
-// //    localEnsemble.predict({'car_kind': 3}, 0,
-// //                          function(error, prediction) {console.log(prediction)});
-
-// var source = new bigml.Source();
-// source.create('BigML_Dataset_610025c9e4279b24a100b7fc.csv', function(error, sourceInfo) {
-//     console.log("sdfjhkjgdhflfjlgkdh")
-//   if (!error && sourceInfo) {
-//     var dataset = new bigml.Dataset();
-//     dataset.create(sourceInfo, function(error, datasetInfo) {
-//       if (!error && datasetInfo) {
-//         var model = new bigml.Model();
-//         model.create(datasetInfo, function (error, modelInfo) {
-//           if (!error && modelInfo) {
-//             var prediction = new bigml.Prediction();
-//             prediction.create(modelInfo, {'car_kind':3})
-//           }
-//         });
-//       }
-//     });
-//   }
-// });
-
-
-
-// var bigml = require('bigml');
-//     var source = new bigml.Source();
-//     source.create('exportdatacsv.csv', function(error, sourceInfo) {
-//       if (!error && sourceInfo) {
-//         var dataset = new bigml.Dataset();
-//         dataset.create(sourceInfo, function(error, datasetInfo) {
-//           if (!error && datasetInfo) {
-//             var model = new bigml.Model();
-//             model.create(datasetInfo, function (error, modelInfo) {
-//               if (!error && modelInfo) {
-//                 var prediction = new bigml.Prediction();
-//                 prediction.create(modelInfo, {"car_number": event.car_number,
-//                 "event_kind": event.event_kind,
-//                 "direction": event.direction,
-//                 "car_kind": event.car_kind,
-//                 "day": event.day,
-//                 "time": event.time,
-//                 "is_special_day": event.is_special_day,
-//                 "first_section": event.first_section,
-//                 "section": event.section})
-//                 console.log(modelInfo+"model info")
-                
-//               }
-//             });
-//           }
-//         });
-//       }
-//     });
